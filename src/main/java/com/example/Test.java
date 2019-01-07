729ca3164e489b5a63417096efc9ae96da94c24d
yiyazhou <yiyazhou@apusapps.com>
2019-01-07 21:21:18 +0800

    Init commit
    Signed-off-by: yiyazhou <yiyazhou@apusapps.com>

---
+++ src/main/java/com/example/Test.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class Test {
+    public static void main(String[] args) {
+        new AppCompatActivity();
+    }
+}
